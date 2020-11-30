eed7acd475aba07235f43b49aaeda6be2ca83297
pikokr <parangee9706@gmail.com>
2020-11-30 21:43:05 +0900

    init

--- src/main/kotlin/com/github/pikokr/sample/plugin/Plugin.kt
+++
@@ -1,6 +0,0 @@
-package com.github.pikokr.sample.plugin
-
-import org.bukkit.plugin.java.JavaPlugin
-
-class Plugin : JavaPlugin() {
-}
---
+++ src/main/kotlin/com/github/pikokr/teaminv/plugin/TeamInventory.kt
@@ -0,0 +1,6 @@
+package com.github.pikokr.teaminv.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TeamInventory : JavaPlugin() {
+}
